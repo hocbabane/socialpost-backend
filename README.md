61cb33d396c33ba41d6dd6d18b903f90a532e03c
Haroun Ayotunde <129394388+hocbabane@users.noreply.github.com>
2023-08-17 02:21:28 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Installation and usage
+create a folder named 'socialpost' and under that folder
+1. Clone this repository (also frontend --> https://github.com/hocbabane/socialpost-frontend.git )
+```
+git clone https://github.com/hocbabane/socialpost-backend.git
+```
+2. Install dependencies
+```
+npm install
+```
+3. Create .env in root directory
+```
+cd ..
+touch .env
+```
+4. Configure environment variables in your new .env file
+```
+MONGO_URI=<YOUR_MONGO_URI> 
+TOKEN_KEY=<YOUR_TOKEN_KEY>
+```
+5. Start a new terminal and run server
+```
+nodemon index.js
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+const connectToMongo = require('./db');
+require('dotenv').config()
+connectToMongo();
+var bodyParser = require("body-parser");
+var cors = require('cors') 
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.json());
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/users', require('./routes/users'))
+app.use("/api/posts", require('./routes/posts'));
+app.use("/api/comments", require('./routes/comments'));
+app.use("/api/messages", require('./routes/messages'));
+
+
+app.listen(port, () => {
+  console.log(`connected: port: ${port}`)
+})
